dcd0d3db8e59dce7d52f9ed1abc439753200c47b
ponguenewende <jessblessed226@gmail.com>
2025-10-30 15:02:01 +0100

    Normalize line endings for Unity project

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerController : MonoBehaviour
+{
+    public int Speed = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Speed * 40 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PunScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class PunScript : MonoBehaviourPunCallbacks
+{
+    public TextMeshProUGUI TxtInfo;
+
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true; // Synchronise la scène entre joueurs
+        PhotonNetwork.GameVersion = "1.0";           // Version du jeu
+        PhotonNetwork.ConnectUsingSettings();        // Connexion avec les paramètres du serveur Photon
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        TxtInfo.text = "Connecté au serveur ! Rejoint ou crée la salle...";
+        PhotonNetwork.JoinOrCreateRoom("WarRoom", new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        string masterStatus = PhotonNetwork.IsMasterClient ? " (You are the Master)" : " (You are not the Master)";
+        TxtInfo.text = $"Connecté à la salle : {PhotonNetwork.CurrentRoom.Name}\n" +
+                       $"Joueurs : {PhotonNetwork.CurrentRoom.PlayerCount}\n" +
+                       $"Rôle : {masterStatus}";
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        string masterStatus = PhotonNetwork.IsMasterClient ? "(You are the Master)" : "(You are not the Master)";
+        TxtInfo.text = $"Un nouveau joueur a rejoint : {newPlayer.NickName}\n" +
+                       $"Joueurs : {PhotonNetwork.CurrentRoom.PlayerCount}\n" +
+                       $"Rôle : {masterStatus}";
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        // ✅ Cet événement manquait ! Il gère la déconnexion d’un joueur de la salle.
+        string masterStatus = PhotonNetwork.IsMasterClient ? "(You are the Master)" : "(You are not the Master)";
+        TxtInfo.text = $"Le joueur {otherPlayer.NickName} a quitté la salle.\n" +
+                       $"Joueurs restants : {PhotonNetwork.CurrentRoom.PlayerCount}\n" +
+                       $"Rôle : {masterStatus}";
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        TxtInfo.text = $"Déconnecté : {cause}";
+    }
+
+    void Update()
+    {
+        // Optionnel : mise à jour continue de l’état réseau
+        if (PhotonNetwork.InRoom)
+        {
+            string masterStatus = PhotonNetwork.IsMasterClient ? "(You are the Master)" : "(You are not the Master)";
+            TxtInfo.text = $"Salle : {PhotonNetwork.CurrentRoom.Name}\n" +
+                           $"Joueurs : {PhotonNetwork.CurrentRoom.PlayerCount}\n" +
+                           $"Rôle : {masterStatus}";
+        }
+        else
+        {
+            TxtInfo.text = PhotonNetwork.NetworkClientState.ToString();
+        }
+    }
+}
