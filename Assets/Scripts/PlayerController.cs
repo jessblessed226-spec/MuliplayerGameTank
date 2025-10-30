454bf6634df33688a0dc016a91f17ff7f2237899
ponguenewende <jessblessed226@gmail.com>
2025-10-30 20:41:05 +0100

    version de player controller avec gestion des instances de tank independants

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,23 @@ using Photon.Realtime;
 
 public class PlayerController : MonoBehaviour
 {
+    PhotonView view;
     public int Speed = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        view = GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime);
-        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Speed * 40 * Time.deltaTime);
+        if (view.IsMine)
+        {
+            transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Speed * Time.deltaTime);
+            transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Speed * 40 * Time.deltaTime);
+        }
+        
     }
 }
